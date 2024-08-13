d82b873e3f25ef13b4c3a900cf59ca8e6e1a1442
estrela801 <rohenri0808@gmail.com>
2024-08-13 15:27:12 -0300

    feat: criando pequeno servidor com o node e express

--- node/index.js
+++ node/index.js
@@ -1,9 +1,22 @@
-const div = require("./div")
-const multi = require("./multi")
-const soma= require("./soma")
-const sub = require("./sub")
+const express = require('express')
+const { dirname } = require('path')
+const app = express()
 
-console.log(div(10,2))
-console.log(multi(10,2));
-console.log(soma(10,2));
-console.log(sub(10,2));
+app.get('/', (req,res)=>{
+    res.send('Deu certo')
+})
+app.get('/index', (req,res)=>{
+    res.sendFile(__dirname + '/html/index.html')
+})
+app.get('/eu', (req,res)=>{
+    res.send('Deu certo, eu sou gostoso')
+})
+app.get('/ola', (req,res)=>{
+    res.send('Deu certo, ola')
+})
+
+app.listen(1112, ()=>{
+    console.log('Foi');
+    
+})
+
