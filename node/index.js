96b3c33d3c324f5409c0827c18a10220c5c6d2f1
estrela801 <rohenri0808@gmail.com>
2024-08-13 14:55:13 -0300

    feat: criação de uma pasta para o projeto e testes de modulo

---
+++ node/index.js
@@ -0,0 +1,9 @@
+const div = require("./div")
+const multi = require("./multi")
+const soma= require("./soma")
+const sub = require("./sub")
+
+console.log(div(10,2))
+console.log(multi(10,2));
+console.log(soma(10,2));
+console.log(sub(10,2));
