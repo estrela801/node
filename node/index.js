e557a944522b3bc4fbd9b4b19ee35ec728ede70f
estrela801 <rohenri0808@gmail.com>
2024-08-14 18:36:21 -0300

    feat: aprendendo a minupular formularios com o Body-Parser

--- node/index.js
+++ node/index.js
@@ -1,23 +1,39 @@
 const express = require('express')
 const { dirname } = require('path')
 const app = express()
-const handlebars = require('express-handlebars')
-
-// Conexão coom obanco de dados 
-    const Sequelize = require('sequelize')
-    const sequelize = new Sequelize('cadastro', 'root', 'estrela801rodrigo187', {
-        host : 'localhost',
-        dialect : 'mysql'
-    })
-    // Configurando a template engine
-     app.engine('handlebars', handlebars({defaultLayout: 'main'}))
-     app.set('view engine', 'handlebars')
+const {   create  } = require('express-handlebars')
+const hbs = create({default: 'main'})
+const bodyParser = require('body-parser')
 
 
 
-// Definindo porta de acesso
-app.listen(1112, ()=>{
-    console.log('Foi');
+//CONFGURAÇÕES DO PROJETO
+    // CONEXÃO COM O BANDO DE DADOS MySql 
+        const Sequelize = require('sequelize')
+        const sequelize = new Sequelize('cadastro', 'root', 'estrela801rodrigo187', {
+            host : 'localhost',
+            dialect : 'mysql'
+        })
+    // CONFIGURANDO TEMPLATE ENGINE'
+        app.engine('handlebars', hbs.engine)
+        app.set('view engine', 'handlebars')
+    // Body-Parser
+        app.use(bodyParser.urlencoded({extended: false}))
+        app.use(bodyParser.json())
+
+
+
+// CONFIGURANDO ROTAS 
+
+app.get('/cadastro', (req,res) =>{
+    res.render('formulario')
+})
+app.post('/foiadd', (req,res)=>{
+
+    res.send("formulario recebido <br>" + 'Titulo = '+ req.body.titulo +'<br> Conteúdo = '+req.body.conteudo)
+})
+app.listen(3000, ()=>{
+    console.log('SERVIDOR RODANDO');
     
 })
 
